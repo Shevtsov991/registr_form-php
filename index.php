27352a204c7479ae952f916a52cc01450e6c0dd9
Roman Shevtsov <Shevtsov.99@mail.ru>
2024-06-01 21:11:59 +0300

    commit

--- index.php
+++ index.php
@@ -1,23 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <script src="https://smartcaptcha.yandexcloud.net/captcha.js" defer></script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Главная</title>
-    <link href="./src/css/style.css" rel="stylesheet" type="text/css" media="all"/>  
+    <link href="./src/css/style.css" rel="stylesheet" type="text/css" media="all"/>    
+    <script src="https://captcha-api.yandex.ru/captcha.js" defer></script>
 </head>
 <body>
-
 <div class="form">
         <h1 class="titel">Войти<h1>
-        <form action="./src/login.php" method="post">  
+        <form action="./src/login.php" method="post" class="loginForm">  
             <input type="text" placeholder="EMAIL или телефон " name="login">
             <input type="password" placeholder="Введите пароль" name="password"> 
-            <button type="submit" >Войти</button>
+            <button type="button" class="loginBtn">Войти</button>
         </form>
     </div>
- 
+
     <div class="form">
         <h1 class="titel">Зарегистрироваться<h1>
         <form action="./src/register.php" method="post">
@@ -28,16 +27,31 @@
             <input type="password" placeholder="Введите пароль" name="pass">
             <input type="password" placeholder="Повторите пароль" name="repPass">
             </div>
-            <button type="submit">Отправить</button>
+            <button type="button" class="regBtn">Отправить</button>
         </form>
     </div>
 
-        <!-- <div;
-            style="height: 100px";
-            id="captcha-container";
-            class="smart-captcha";
-            data-sitekey="ysc1_Pc5BL8D5ew910A722f6HPe65GczkGXB4piQFFwe887745e6f";
-        ></div>; -->
+    <div
+    class="smart-captcha"
+    data-sitekey="ysc1_Pc5BL8D5ew910A722f6HPe65GczkGXB4piQFFwe887745e6f"
+    data-hl="ru"
+    data-callback="yandex_captcha"
+    ></div>
 
+
+    <!-- скрипт капчи -->
+<script>
+
+    function yandex_captcha() {
+        let smart_token = document.querySelector('input[name="smart-token"]').value;
+        const loginBtn = document.querySelector('.loginBtn')
+        const regBtn = document.querySelector('.regBtn')
+        // alert
+        smart_token !== '' ? (loginBtn.type='submit', regBtn.type='submit') : false
+        
+    }
+
+</script>
+    <!-- скрипт капчи  -->
 </body>
 </html>
--- src/register.php
+++ src/register.php
@@ -38,42 +38,7 @@ if (empty($name)|| empty($email)||empty($tel)||empty($pass)|| empty($repPass)) {
             echo " регистрация проша успешно!" . '<br/>';
             echo $back . '<br/>';
             echo "<a href ='./user-panel.php?email=$email'><button>Войти</button></a>";
-            /*kaptcha */
-            // define('SMARTCAPTCHA_SERVER_KEY', '');
-            
-               
-                // function check_captcha($token) {
-                //     $ch = curl_init();
-                //     $args = http_build_query([
-                //         "secret" => SMARTCAPTCHA_SERVER_KEY,
-                //         "token" => $token,
-                //         "ip" => $_SERVER['REMOTE_ADDR'], // Нужно передать IP пользователя.
-                //                                         // Как правильно получить IP зависит от вашего прокси.
-                //     ]);
-                //     curl_setopt($ch, CURLOPT_URL, "https://smartcaptcha.yandexcloud.net/validate?$args");
-                //     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                //     curl_setopt($ch, CURLOPT_TIMEOUT, 1);
-
-                //     $server_output = curl_exec($ch);
-                //     $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-                //     curl_close($ch);
-
-                //     if ($httpcode !== 200) {
-                //         echo "Allow access due to an error: code=$httpcode; message=$server_output\n";
-                //         return true;
-                //     }
-                //     $resp = json_decode($server_output);
-                //     return $resp->status === "ok";
-                // }
-
-                // $token = $_POST['smart-token'];
-                // if (check_captcha($token)) {
-                //     echo "Passed\n";
-                // } else {
-                //     echo "Robot\n";
-                // }
-            /*kaptcha */
-
+           
         }
         else{
             echo "Ошибка: " . $connection ->error. '<br/>';
